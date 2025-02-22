90ace1a6b94a9e0419d1ef1b968aa7bbd3c72d88
Clara Bordeau <cbordeau@paul-f4Br8s2.clusters.42paris.fr>
2025-02-22 12:14:41 +0100

    funcheck ok

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: cbordeau <cbordeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 11:58:05 by cbordeau          #+#    #+#             */
-/*   Updated: 2025/02/22 09:29:49 by cbordeau         ###   ########.fr       */
+/*   Updated: 2025/02/22 12:01:15 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_exit(t_data *fdf, t_list *lst, int err_code)
 		ft_lstclear(&lst, free);
 	liberator_int_tab(fdf->coordinate, fdf->maxy);
 	liberator_int_tab(fdf->save, fdf->maxy);
-	if (fdf->img)
-		mlx_destroy_image(fdf->mlx, fdf->img);
-	if (fdf->win)
-		mlx_destroy_window(fdf->mlx, fdf->win);
 	if (fdf->mlx)
 	{
+		if (fdf->img)
+			mlx_destroy_image(fdf->mlx, fdf->img);
+		if (fdf->win)
+			mlx_destroy_window(fdf->mlx, fdf->win);
 		mlx_destroy_display(fdf->mlx);
 		free(fdf->mlx);
 	}
--- srcs/parsing_utils.c
+++ srcs/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbordeau <cbordeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 12:24:41 by cbordeau          #+#    #+#             */
-/*   Updated: 2025/02/22 09:36:38 by cbordeau         ###   ########.fr       */
+/*   Updated: 2025/02/22 11:59:55 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,8 @@ void	liberator_int_tab(t_coordinate **tab, int line)
 	int	i;
 
 	i = 0;
+	if (!tab)
+		return ;
 	while (i < line)
 	{
 		free(tab[i]);
